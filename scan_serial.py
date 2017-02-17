a66fe7b817d338c14e11a0b498f05296767faadc
Jense Arntz <jense_arntz@mail.com>
2017-02-17 19:12:03 +0100

    initial push.

--- scan_serial.py
+++ scan_serial.py
@@ -6,6 +6,6 @@ while True:
 	buffer = fp.read(8)
 	for c in buffer:
 		if ord(c) > 0:
-			print 'Output: {}'.format(ord(c))
-
+			print 'Output: {}\r'.format(ord(c))
+			print 'Output: {}\r'.format(c)
 	print('\n')
