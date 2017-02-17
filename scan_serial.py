0d99ce781988373c420241fac03a4f6500b03712
Jense Arntz <jense_arntz@mail.com>
2017-02-17 19:09:43 +0100

    initial push.

--- scan_serial.py
+++ scan_serial.py
@@ -1,16 +1,3 @@
-import os, sys
-import serial
-import time
-# ser = serial.Serial('/dev/hidraw0', 19200, timeout=5)
-#
-# while True:
-# 	line = ser.readline()
-# 	if len(line) == 0:
-# 		print("Timeout ! Exit. \n")
-# 		sys.exit()
-# 	print line
-#
-
 import sys
 
 fp = open('/dev/hidraw0', 'rb')
@@ -19,5 +6,6 @@ while True:
 	buffer = fp.read(8)
 	for c in buffer:
 		if ord(c) > 0:
-			print ord(c)
-	print "\n"
+			print 'Output: {}'.format(ord(c))
+
+	print('\n')
