aec5b989785df8716126c00e9f060b46c8bc13cc
Jense Arntz <jense_arntz@mail.com>
2017-03-16 14:10:04 +0100

    add write_rfid_v1

--- cottonwood/protocol.py
+++ cottonwood/protocol.py
@@ -26,6 +26,22 @@ def check_sum(cmd):
 	return sum
 
 
+def convert_int(data):
+	"""
+	Convert String to Int
+	:param data:
+	:return:
+	"""
+	array = []
+	for i in data:
+		array.append(int(i))
+
+	while len(array) < 12:
+		array.insert(0, 0x00)
+
+	return array
+
+
 def protocol_packet(TYPE, CMD, DATA=[]):
 	"""
 	:param TYPE:
@@ -42,14 +58,14 @@ def protocol_packet(TYPE, CMD, DATA=[]):
 
 
 def epc_tag_identy():
-	return protocol_packet(SEND_TYPE, 0x82, [0x00])
+	return protocol_packet(SEND_TYPE, 0x82, DATA=[0x00])
 
 
 def epc_tag_read():
-	return protocol_packet(SEND_TYPE, 0x80, [0x06, 0x80, 0x00, 0x01, 0x02, 0x01])
+	return protocol_packet(SEND_TYPE, 0x80, DATA=[0x06, 0x80, 0x00, 0x01, 0x02, 0x01])
 
 
 def epc_tag_write_multi(badge):
 	data = [0x00, 0x01, 0x01, 0x02, 0x06]
-	data += badge
-	return protocol_packet(SEND_TYPE, 0x81, data)
+	data += convert_int(badge)
+	return protocol_packet(SEND_TYPE, 0x81, DATA=data)
