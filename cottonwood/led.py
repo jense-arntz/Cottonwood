c4aa5bdac3590187499fbc9ddc3239e49e2a7c70
Jense Arntz <jense_arntz@mail.com>
2017-04-04 21:53:41 +0200

    Fix brightness

--- cottonwood/led.py
+++ cottonwood/led.py
@@ -5,10 +5,10 @@ from time import sleep
 
 def blink():
 	for i in range(1, 4):
-		r = randint(0, 255)
-		g = randint(0, 255)
-		b = randint(0, 255)
-		set_all(r, g, b, brightness=0.5)
+		r = 255
+		g = 0
+		b = 0
+		set_all(r, g, b, brightness=1)
 		show()
 		sleep(1)
 		clear()
